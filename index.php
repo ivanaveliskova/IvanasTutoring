9389ae08a6a508dd47b52c4aaed8a66bc6f2cc98
smidra <ivanaveliskova@gmail.com>
2015-01-27 23:12:13 -0500

    trying form stuffs....

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Tutoring Site'; 
+    $to = 'ivanas.tutoring@gmail.com'; 
+    $subject = 'In Need of a Tuto';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    if ($_POST['submit'] && $human == '4') {
+	    if (mail ($to, $subject, $body, $from)) { 
+	        echo '<p>Your message has been sent!</p>';
+	    } else { 
+	        echo '<p>Something went wrong, go back and try again!</p>'; 
+	    }
+	}
+?>
