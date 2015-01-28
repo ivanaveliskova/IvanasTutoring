b6a74179a3a592a3d252e5f94c850e90b8beae8e
smidra <ivanaveliskova@gmail.com>
2015-01-27 23:54:36 -0500

    trying so hard....

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $message = $_GET['message'];
     $from = 'From: Tutoring'; 
     $to = 'ivanaveliskova@gmail.com'; 
     $subject = 'Hello';
-    $human = $_POST['human'];
 			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 				
-    if ($_POST['submit'] && $human == '4') {				 
-        if (mail ($to, $subject, $body, $from)) { 
+				 
+    if (mail ($to, $subject, $body, $from)) { 
 	    echo '<p>Your message has been sent!</p>';
 	} else { 
 	    echo '<p>Something went wrong, go back and try again!</p>'; 
 	} 
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
-    }
 ?>
